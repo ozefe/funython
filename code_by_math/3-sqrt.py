9e512acd637c576c65ab6e81cd7f4968f4350f51
ozefe <gh@efe.cv>
2024-06-12 23:21:09 +0300

    Finding the square root of any positive integer
    Signed-off-by: ozefe <gh@efe.cv>

---
+++ code_by_math/3-sqrt.py
@@ -0,0 +1,6 @@
+# A program to find the square root of any positive integer[0][1]
+#
+# [0] - https://stackoverflow.com/questions/17417490/difference-between-sqrtx-and-powx-0-5
+# [1] - https://www.codebymath.com/index.php/welcome/challenge/square-root
+
+print(f"sqrt({(n := input('=> '))}) \u2245 {int(n)**.5}")
