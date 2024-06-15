8fa675db20167966dde338e4c0c9a886c07f6311
ozefe <gh@efe.cv>
2024-06-15 18:41:44 +0300

    Add digits of an integer using mod
    Signed-off-by: ozefe <gh@efe.cv>

---
+++ code_by_math/6-add_digits.py
@@ -0,0 +1,11 @@
+# A program to add digits of the given integer using the mod-operator[0]
+#
+# [0] - https://www.codebymath.com/index.php/welcome/challenge/add-digits
+
+print(str(l :=   lambda _         , __  :l(
+_           :=            _         //  10,
+__          :=              __      +[   _,
+]) if _ . __gt__( 0*_*0 ) else __ ) *-  31,
+(I := int(input('>>> '))).__str__() *-  69,
+f'<<< {sum( _ % 10 for _ in l(I,[I] ))  }',
+sep='')#aW50MmJpbiB1c2luZyBtb2QgYnkgb3plZmU
